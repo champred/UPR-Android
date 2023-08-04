918195e7a7999e33ae8d87655545908c8ed91d16
champred <championred69@gmail.com>
2023-08-04 12:23:49 -0600

    Add GSC/doubles presets

--- app/src/main/kotlin/ly/mens/rndpkmn/settings/SettingsPreset.kt
+++ app/src/main/kotlin/ly/mens/rndpkmn/settings/SettingsPreset.kt
@@ -9,12 +9,14 @@ enum class SettingsPreset {
 	STANDARD,
 	ULTIMATE,
 	KAIZO,
-	SURVIVAL;
+	SURVIVAL,
+	DOUBLES;
 
 	open val preset: String? get() {
 		val name = RandomizerSettings.romName ?: return null
 		val prefix: String = when (RandomizerSettings.currentGen) {
 			1 -> "RBY"
+			2 -> "GSC"
 			3 -> if ("Fire" in name || "Leaf" in name) "FRLG" else "RSE"
 			4 -> if ("Heart" in name || "Soul" in name) "HGSS" else "DPPT"
 			5 -> if ("2" in name) "B2W2" else "BW"
